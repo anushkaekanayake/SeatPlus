d82c7df5fba9079f3f75821feef027e03cc956c0
anushka <test@aste.com>
2016-09-15 13:05:36 +0700

    adding CURD interface and repositories

---
+++ src/main/java/org/seatplus/repository/UserRepository.java
@@ -0,0 +1,30 @@
+package org.seatplus.repository;
+
+import org.seatplus.CURD.CURD;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by anushka.ekanayake on 9/15/2016.
+ */
+@Repository
+public class UserRepository implements CURD {
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public Object get(Object o) {
+        return null;
+    }
+
+    @Override
+    public void delete(Object o) {
+
+    }
+
+    @Override
+    public Object getAll() {
+        return null;
+    }
+}
