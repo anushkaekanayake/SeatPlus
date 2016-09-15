6b615f8c1de5a1c3c4166c5ef1cab556c91c9163
anushka <test@aste.com>
2016-09-15 13:04:42 +0700

    updates for models

--- src/main/java/org/seatplus/models/Message.java
+++ src/main/java/org/seatplus/models/Message.java
@@ -1,8 +1,11 @@
 package org.seatplus.models;
 
+import javax.persistence.Entity;
+
 /**
  * Created by anushka.ekanayake on 9/7/2016.
  * Message model = uses when sending sms messages
  */
+@Entity
 public class Message {
 }
--- src/main/java/org/seatplus/models/Schedule.java
+++ src/main/java/org/seatplus/models/Schedule.java
@@ -1,7 +1,10 @@
 package org.seatplus.models;
 
+import javax.persistence.Entity;
+
 /**
  * Created by anushka.ekanayake on 9/5/2016.
  */
+@Entity
 public class Schedule {
 }
